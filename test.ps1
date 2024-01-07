f0d38b0c54aa9f61719e7d4b7f12dec6f22f19d4
Marcus Mayorga <marcusalexander1999@gmail.com>
2024-01-06 21:03:58 -0500

    main
    Signed-off-by: Marcus Mayorga <marcusalexander1999@gmail.com>

--- test.ps1
+++ test.ps1
@@ -1,24 +1,3 @@
-# Creado por: Marcus Mayorga
-
-<#
-Este Script se usa para mover las capturas de la apliacion de Xbox del almacenamiento principal a otra
-ubicacion en caso de no querer usar OneDrive o el disco principal
-#>
-
-# Ruta de origen
-$origen = "C:\Users\marcu\Videos\Captures"
-
-# Verificar si hay archivos en la carpeta de origen
-if (!(Test-Path $origen) -or !(Get-ChildItem $origen)) {
-    # Mostrar notificación en Windows de que no hay archivos para mover
-    if (Get-Command "New-BurntToastNotification" -ErrorAction SilentlyContinue) {
-        New-BurntToastNotification -Text "No se pueden mover archivos de la carpeta de origen porque se encuentra vacia." -AppLogo E:\Windows\Recurso-1.ico
-    } else {
-        Write-Host "Para mostrar la notificación, instala el módulo BurntToast."
-    }
-    exit
-}
-
 # Ruta de origen
 $origen = "C:\Users\marcu\Videos\Captures"
 
